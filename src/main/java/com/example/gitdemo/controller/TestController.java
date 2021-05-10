a99790284f8889c3acc4b5c2604129db56196548
网销-彭志全 <kevin.peng@cignacmb.com>
2021-05-10 15:29:43 +0800

    修改代码

--- src/main/java/com/example/gitdemo/controller/TestController.java
+++ src/main/java/com/example/gitdemo/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.example.gitdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@Slf4j
+@RestController()
+@RequestMapping("/test")
 public class TestController {
 
     @RequestMapping("/getData")
     public String getMethod(){
+        log.info("获取到了数据");
         return "获取到了数据";
     }
 }
